023efff729191c0f3009e795651ab4b042800ab3
Nathan Tornquist <Tornquist@live.com>
2018-08-10 18:55:54 -0500

    Initial Commit

---
+++ LudumDare42/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LudumDare42
+//
+//  Created by Nathan Tornquist on 8/10/18.
+//  Copyright © 2018 nathantornquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
