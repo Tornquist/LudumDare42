553ddf50e22df94637b25396a55a1f719b1d4c60
Nathan Tornquist <Tornquist@live.com>
2018-08-11 18:24:37 -0500

    Add Menu (inactive)

---
+++ LudumDare42/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  LudumDare42
+//
+//  Created by Nathan Tornquist on 8/11/18.
+//  Copyright © 2018 nathantornquist. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+}
